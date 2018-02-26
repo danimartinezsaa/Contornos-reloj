4713ec09878fab1d4976ff8dbccda39cfa03485b
Daniel Martinez <dani@danielcastelao.org>
2018-02-26 09:23:16 +0100

    Añadido JavaDoc.

--- src/contornosreloj/ContornosReloj.java
+++ src/contornosreloj/ContornosReloj.java
@@ -1,24 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package contornosreloj;
 
+package contornosreloj;
 import java.awt.Toolkit;
 import java.util.TimerTask;
 import java.util.Timer;
 
-
-
 /**
- *
+ * Aplicación de un reloj digital con alarma.
+ * En la clase principal se llama a la interfaz, se gestiona la alarma y se incluye una InnerClass para gestionar el reloj en segundo plano.
  * @author dani
  */
 public class ContornosReloj{
     Timer timer;
     String hora;
-    
+    /**
+     * Clase main dónde se instancian el reloj y la alarma, se muestra la hora de ambos y se lanza la alarma
+     * @param args
+     * @throws InterruptedException 
+     */
     public static void main(String[] args) throws InterruptedException{
         boolean encendido=true;
         Interfaz ventana=new Interfaz();
@@ -32,27 +30,27 @@ public class ContornosReloj{
             else
                 Interfaz.mostrarAlarma();
             
-            if(Reloj.getHoracompleta().equals(Alarma.getHoracompleta())&&Alarma.activada==true){
+            if(Reloj.horacompleta.equals(Alarma.getHoracompleta())&&Alarma.activada==true){
                 System.out.println("Alarma");
                 toolkit.beep();
                 Thread.sleep(1000);
             } 
         }
     }
-    
+    /**
+     * Constructor de la clase principal para lanzar el TimerTask.
+     */
     public ContornosReloj(){
         timer=new Timer();
         timer.schedule(new TicTac(), 0, 1000);
     }
-    
+    /**
+     * Clase que se ejecuta en segundo plano para gestionar el incremento del reloj.
+     */
     public class TicTac extends TimerTask {
-
-        Toolkit toolkit = Toolkit.getDefaultToolkit();
-
         @Override
         public void run() {    
             Reloj.incrementoHora();
-            System.out.println(Reloj.getHoracompleta());
         }
     }
 }
